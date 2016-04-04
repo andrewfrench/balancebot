8843d6ba3a93a90dd5b01479225f4bee21dfcbc2
Andrew French <andrew@afren.ch>
2016-04-04 13:04:45 -0600

    Code ready for balance experiments.

--- main/Motors.h
+++ main/Motors.h
@@ -1,12 +1,20 @@
 #include <Encoder.h>
 #include <DualMC33926MotorShield.h>
 
+#define COUNTS_PER_REV  3200
+
 class Motors {
   public:
     Motors(int, int, int, int);
     void manualUpdate(int, int);
     void stopRobot(void);
+    float getLeftEncoderVelocity(void);
+    float getRightEncoderVelocity(void);
   private:
+    long lastCountLeft;
+    long lastCountRight;
+    long lastCountTimeLeft;
+    long lastCountTimeRight;
     Encoder * leftEncoder;
     Encoder * rightEncoder;
     DualMC33926MotorShield motorShield;
@@ -15,6 +23,11 @@ class Motors {
 Motors::Motors(int lp0, int lp1, int rp0, int rp1) {
   *leftEncoder = Encoder(lp0, lp1); // Encoder(18, 19);
   *rightEncoder = Encoder(rp0, rp1); // Encoder(2, 3);
+
+  lastCountLeft = 0;
+  lastCountRight = 0;
+  lastCountTimeLeft = micros();
+  lastCountTimeRight = micros();
 
   motorShield.init();
   motorShield.setM1Speed(0);
@@ -29,4 +42,60 @@ void Motors::manualUpdate(int left, int right) {
 void Motors::stopRobot() {
   motorShield.setM1Speed(0);
   motorShield.setM2Speed(0);
+}
+
+float Motors::getLeftEncoderVelocity() {
+  // Get current reading time [usec]
+  long currentTimeLeft = micros();
+
+  // Get current encoder count
+  long leftCount = leftEncoder->read();
+
+  // Get the difference in encoder counts since last reading
+  long countDiffLeft = leftCount - lastCountLeft;
+
+  // Find the number of revolutions since last reading
+  float revsLeft = countDiffLeft / COUNTS_PER_REV;
+
+  // Convert revolutions to radians
+  float radsLeft = revsLeft * 2 * 3.1415;
+
+  // Find the time difference between readings
+  long timeDiffLeft = currentTimeLeft - lastCountTimeLeft;
+
+  // Replace last time reading with current time reading
+  lastCountTimeLeft = currentTimeLeft;
+
+  // Convert radians to radians/sec, mult by 1e6 to convert from usec to sec
+  float radsPerSecLeft = (radsLeft / timeDiffLeft) * 1000000;
+
+  return radsPerSecLeft;
+}
+
+float Motors::getRightEncoderVelocity() {
+  // Get current reading time [usec]
+  long currentTimeRight = micros();
+
+  // Get current encoder count
+  long rightCount = rightEncoder->read();
+
+  // Get the difference in encoder counts since last reading
+  long countDiffRight = rightCount - lastCountRight;
+
+  // Find the number of revolutions since last reading
+  float revsRight = countDiffRight / COUNTS_PER_REV;
+
+  // Convert revolutions to radians
+  float radsRight = revsRight * 2 * 3.1415;
+
+  // Find the time difference between readings
+  long timeDiffRight = currentTimeRight - lastCountTimeRight;
+
+  // Replace last time reading with current time reading
+  lastCountTimeRight = currentTimeRight;
+
+  // Convert radians to radians/sec, mult by 1e6 to convert from usec to sec
+  float radsPerSecRight = (radsRight / timeDiffRight) * 1000000;
+
+  return radsPerSecRight;
 }
--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,7 @@
 // #include "Communication.h"
 // #include "Ultrasonic.h"
 // #include "Colors.h"
-// #include "Motors.h"
+#include "Motors.h"
 #include "Accelerometer.h"
 
 /*
@@ -27,8 +27,8 @@ The top-level control file.
 // Define motor control configuration values
 #define LEFT_ENC_PIN_0                 18
 #define LEFT_ENC_PIN_1                 19
-#define RIGHT_ENC_PIN_0                2
-#define RIGHT_ENC_PIN_1                3
+#define RIGHT_ENC_PIN_0                20
+#define RIGHT_ENC_PIN_1                21
 
 // Define accelerometer configuration values
 #define ACCEL_X_X1_PIN                 A2
@@ -38,19 +38,24 @@ The top-level control file.
 #define ACCEL_HP_PIN                   2
 
 Accelerometer * accelerometer;
+Motors * motors;
 
 void setup() {
   Serial.begin(115200);
   // Communication communication(COMMUNICATION_CE_PIN, COMMUNICATION_CS_PIN);
   // Ultrasonic ultrasonic(ULTRASONIC_OBSTACLE_THRESHOLD, ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN);
   // Colors colors(COLORS_FINISH_LINE_THRESHOLD);
-  // Motors motors(LEFT_ENC_PIN_0, LEFT_ENC_PIN_1, RIGHT_ENC_PIN_0, RIGHT_ENC_PIN_1);
+  motors = new Motors(LEFT_ENC_PIN_0, LEFT_ENC_PIN_1, RIGHT_ENC_PIN_0, RIGHT_ENC_PIN_1);
   accelerometer = new Accelerometer(ACCEL_X_X1_PIN, ACCEL_X_X4_PIN, ACCEL_Z_X1_PIN, ACCEL_Z_X4_PIN, ACCEL_HP_PIN);
 }
 
 void loop() {
-  Serial.print("X: ");
+  Serial.print("Tu,");
+  Serial.print(micros());
+  Serial.print(",Xa,");
   Serial.print(accelerometer->getX1());
-  Serial.print(" Z: ");
-  Serial.println(accelerometer->getZ1());
+  Serial.print(",Za,");
+  Serial.print(accelerometer->getZ1());
+  Serial.print(",Ev,");
+  Serial.print(motors->getLeftEncoderVelocity());
 }
